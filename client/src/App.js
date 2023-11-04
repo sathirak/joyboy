2203eb77df3fdc6672ca428cddd1fe57eb37f7fa
sekisaii <145209193+sekisaii@users.noreply.github.com>
2023-11-04 19:38:12 +0530

    Newly refreshed

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Food from './pages/Food';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Food" element={<Food />} />
       </Routes>
     </Router>
   );
