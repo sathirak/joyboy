e42b4ed07741ead75086806bdd106b840e1cb88a
sekisaii <145209193+sekisaii@users.noreply.github.com>
2023-11-29 07:56:29 +0000

    nya1~~

--- client-moby/src/keiko/Keiko.js
+++ client-moby/src/keiko/Keiko.js
@@ -1,22 +1,24 @@
 import React, { useState} from 'react';
-import {SidebarButton} from './keiko-icons/sidebar_button.svg';
 import './Keiko.css';
 
+import sidebarButton from './keiko-icons/sidebar_button.svg';
+
 function Keiko() {
-    const [sidebar,setSideBar] = useState(null);
+    const [sidebar,setSideBar] = useState(false);
 
     return (
         <div className='Keiko'>
-          <div className='Keiko-Sidebar-Button'>
-
+          <div className='Keiko-Sidebar-Button-Wrapper'>
+            <img src={sidebarButton} alt='Sidebar Button' className='Keiko-Sidebar-Button' onClick={() => setSideBar(!sidebar)}/>
           </div>
 
-          <div className='Keiko-Sidebar'>
-
-          </div>
+            {sidebar && (
+              <div className='Keiko-Sidebar'>
+              </div>
+            )}
 
           <div className='Keiko-Content'>
-
+            <h4 className='Keiko-Heading'>Keiko Dashboard</h4>
           </div>
         </div>
       );
